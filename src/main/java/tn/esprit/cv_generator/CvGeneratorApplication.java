392d6c4a769ac7363c2ba688e70d03c90318f0f9
Rachdi Mohamed Amine <71773777+zorogiro@users.noreply.github.com>
2026-06-16 02:50:33 +0100

    template

---
+++ src/main/java/tn/esprit/cv_generator/CvGeneratorApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.cv_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvGeneratorApplication.class, args);
+	}
+
+}
